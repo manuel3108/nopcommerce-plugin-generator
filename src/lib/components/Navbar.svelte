1be0b72636fa5ad274d78e5d60e7945886a2d36d
Manuel Serret <mserret99@gmail.com>
2021-12-29 08:14:51 +0100

    setup gh pages

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,43 +2,34 @@
 </script>
 
 <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <div class="navbar-item">
-            <a href="/"><img src="/favicon.png" alt="Plugin Gleisbelegung Logo" /></a>
-            Plugin Gleisbelegung
-        </div>
-    </div>
-    <div class="navbar-menu">
-        <div class="navbar-item">
-            <a href="/installation">Installation</a>
-        </div>
-        <div class="navbar-item">
-            <a href="/faq">FAQ</a>
-        </div>
-        <div class="navbar-item">
-            <a href="/changelog">Changelog</a>
-        </div>
-        <div class="navbar-end">
-            <div class="navbar-item">
-                <a href="https://github.com/gleisbelegung/gleisbelegung">Github</a>
-            </div>
-            <div class="navbar-item">
-                <span>
-                    <!-- {#if $currentTime}
+	<div class="navbar-brand">
+		<div class="navbar-item">
+			<a href="/"><img src="/favicon.png" alt="Plugin Gleisbelegung Logo" /></a>
+			Plugin Gleisbelegung
+		</div>
+	</div>
+	<div class="navbar-menu">
+		<div class="navbar-end">
+			<div class="navbar-item">
+				<a href="https://github.com/gleisbelegung/gleisbelegung">Github</a>
+			</div>
+			<div class="navbar-item">
+				<span>
+					<!-- {#if $currentTime}
 						{$currentTime.toFormat('HH:mm:ss')}
 					{/if} -->
-                </span>
-            </div>
-        </div>
-    </div>
+				</span>
+			</div>
+		</div>
+	</div>
 </nav>
 
 <style>
-    .navbar {
-        border-radius: 0;
-    }
+	.navbar {
+		border-radius: 0;
+	}
 
-    img {
-        margin-right: 10px;
-    }
+	img {
+		margin-right: 10px;
+	}
 </style>
