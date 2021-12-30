f828dc75304b3ba3e7279cdbf90864a751f8ade1
manuel3108 <manuel3108@users.noreply.github.com>
2021-12-30 14:24:57 +0000

    deploy: 2f0119f938c4cceb7469862443eef41ea58796ed

--- _app/pages/index.svelte-0855da6e.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-be60d904.js";import{R as o}from"../chunks/index-e14d7120.js";import"../chunks/preload-helper-ec9aa979.js";export{o as default};
---
+++ _app/pages/index.svelte-ec657bdc.js
@@ -0,0 +1,1 @@
+import"../chunks/vendor-be60d904.js";import{R as o}from"../chunks/index-7659f4a0.js";import"../chunks/preload-helper-ec9aa979.js";export{o as default};
