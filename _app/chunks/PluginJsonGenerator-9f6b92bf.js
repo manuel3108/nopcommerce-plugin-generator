6d0f7b043b49911a594a4f4cd3c6317fe959f1d9
manuel3108 <manuel3108@users.noreply.github.com>
2022-01-01 13:16:39 +0000

    deploy: 097cf7934d32d288eac3579b6ba578d50c32a5c1

---
+++ _app/chunks/PluginJsonGenerator-9f6b92bf.js
@@ -0,0 +1,1 @@
+import o from"./PluginJsonGenerator-9d461a28.js";import"./File-9e84454c.js";import"./index-21292ce4.js";import"./vendor-83d19bf2.js";import"./preload-helper-ec9aa979.js";class l extends o{generatePluginsJsonContent(n){const e=super.generatePluginsJsonContent(n);return e.SupportedVersions=["4.50"],e}}export{l as default};
--- _app/pages/index.svelte-a0fcac1c.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-83d19bf2.js";import{R as a}from"../chunks/index-4e721ff7.js";import"../chunks/preload-helper-ec9aa979.js";export{a as default};
