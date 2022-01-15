21125f2e42cc66cfda2c4ea32f566a8a80fca161
manuel3108 <manuel3108@users.noreply.github.com>
2022-01-15 08:17:49 +0000

    deploy: 9349bfc82ff531e3d0f7e934da68d6bef23297e2

---
+++ _app/chunks/Class-388fd76b.js
@@ -0,0 +1,1 @@
+import{C as r}from"./ClassBase-ef3c650e.js";class o extends r{constructor(s,a,e){super("class",s,a,e)}}export{o as C};
--- _app/chunks/Class-64f78858.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./ClassBase-bd07e1e4.js";class t extends r{constructor(s,a,e){super("class",s,a,e)}}export{t as C};
