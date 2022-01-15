906955c6e5e8fe9d4898ec2832870a99968eb587
manuel3108 <manuel3108@users.noreply.github.com>
2022-01-15 08:14:52 +0000

    deploy: 6ae88a9349bc318cc2e17fdb9ff50a9e689b7e72

---
+++ _app/chunks/Class-770fa420.js
@@ -0,0 +1,1 @@
+import{C as r}from"./ClassBase-968f30af.js";class t extends r{constructor(s,a,e){super("class",s,a,e)}}export{t as C};
--- _app/pages/index.svelte-1e44eeff.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-70f8720b.js";import{R as o}from"../chunks/index-8f328d0a.js";import"../chunks/preload-helper-ec9aa979.js";export{o as default};
