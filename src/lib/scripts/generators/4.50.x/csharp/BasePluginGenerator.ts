39de41ca9fada43977b69ac0590988f34362c07b
Manuel Serret <mserret99@gmail.com>
2022-03-05 11:33:55 +0100

    fix #9

---
+++ src/lib/scripts/generators/4.50.x/csharp/BasePluginGenerator.ts
@@ -0,0 +1,13 @@
+import type PluginConfig from '$lib/scripts/configs/PluginConfig';
+import type { File } from '$lib/scripts/common/File';
+import BasePluginGenerator from '../../4.40.x/csharp/BasePluginGenerator';
+
+export default class BasePluginGenerator450 extends BasePluginGenerator {
+	generate(config: PluginConfig): File[] {
+		const files = super.generate(config);
+		console.log('test');
+		files[0].content = files[0].content.replace('_localizationService.AddLocaleResourceAsync', '_localizationService.AddOrUpdateLocaleResourceAsync');
+
+		return files;
+	}
+}
--- src/lib/scripts/generators/4.50.x/cshtml/ViewImportsGenerator.ts
+++ src/lib/scripts/generators/4.50.x/cshtml/ViewImportsGenerator.ts
@@ -1,18 +1,8 @@
-import { File } from '$lib/scripts/common/File';
 import type PluginConfig from '$lib/scripts/configs/PluginConfig';
 import ViewImportsGenerator from '../../4.40.x/cshtml/ViewImportsGenerator';
-import { addView } from '../../ViewsSingleton';
 
 export default class AdminConfigurationBaseGenerator450 extends ViewImportsGenerator {
-	generate(config: PluginConfig): File[] {
-		const path = ['Areas', 'Admin', 'Views'];
-		const name = '_ViewImports';
-		const extension = 'cshtml';
-		addView(path, name, extension);
-		return [new File(name, extension, path, this.generateContent(config))];
-	}
-
-	protected generateContent(config: PluginConfig): string {
+	protected override generateContent(config: PluginConfig): string {
 		let content = super.generateContent(config);
 
 		content += '\n\n@inject INopHtmlHelper NopHtml';
